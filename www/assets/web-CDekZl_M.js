fabc34948932fbc2d53bde2f5946070b0a89ab33
Usman Ameen <usmanameen832@gmail.com>
2026-07-29 18:49:18 +0500

    removed clear All Button

--- www/assets/web-CDekZl_M.js
+++ www/assets/web-CDekZl_M.js
@@ -1,4 +1,4 @@
-import { W as WebPlugin } from "./index-BAqpjZG_.js";
+import { W as WebPlugin } from "./index-CoSpfFGY.js";
 class ShareWeb extends WebPlugin {
   async canShare() {
     if (typeof navigator === "undefined" || !navigator.share) {
